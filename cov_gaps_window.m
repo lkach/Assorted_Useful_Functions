794570bd96a4c516b10e038c30f2b70134775dd2
Luke Kachelein <lkachele@ucsd.edu>
2021-08-10 05:16:25 -0700

    Update cov_gaps_window.m
    Corrected a fatal error that precluded asymmetric covariances (i.e. all covariances between two different time series).

--- cov_gaps_window.m
+++ cov_gaps_window.m
@@ -11,8 +11,8 @@
 % IN:   Y      = Same size as X, this is for cases where you want the
 %                covariance between two different vectors, not an
 %                autocovariance of X with itself.
-% IN:   maxlag = The maximum number of lags considered. The deafult is
-%                N - 1. For very long X and Y, this needs to be much
+% IN:   maxlag = The maximum number of lags considered. The default is
+%                +-(N - 1). For very long X and Y, this needs to be much
 %                smaller than N - 1, otherwise it could take a long time to
 %                do the calculation (a warning will be issued so that the
 %                user knows if they need to force stop).
@@ -26,11 +26,11 @@
 % Possible outputs:
 % 
 % OUT:  cov_xy     = The covariance between X and Y (or covariance of X).
-%                    Length = maxlag + 1
-% OUT:  lags       = The corresponding lags. Length = maxlag + 1
+%                    Length = 2*maxlag + 1
+% OUT:  lags       = The corresponding lags. Length = 2*maxlag + 1
 % OUT:  cov_std_xy = Standard deviation of points that made each element of
-%                    autocov. Length = maxlag + 1
-% OUT:  n_cov_xy   = 2-column matrix (Length = maxlag + 1):
+%                    autocov. Length = 2*maxlag + 1
+% OUT:  n_cov_xy   = 2-column matrix (Length = 2*maxlag + 1):
 %                    - First is how many pairs were finite for calculating
 %                      the corresponding element in cov_xy
 %                    - Second is how many pairs would have been used if
@@ -82,6 +82,10 @@ elseif nargin == 2 % % % % % % % % % % % % % % % % % % % % % % % % % % % %
         Y = varargin{2};
         maxlag = length(X) - 1;
         Window = 'rectwin';
+    elseif ischar(varargin{2})
+        Y = varargin{1};
+        maxlag = length(X) - 1;
+        Window = varargin{2};
     else
         error('Inputs are incorrectly formatted.')
     end
@@ -129,26 +133,37 @@ N_x = length(X);
 X = X - nanmean(X);
 Y = Y - nanmean(Y);
 
-cov_xy = zeros(1 + maxlag,1); % the autocovariance
-cov_std_xy = zeros(1 + maxlag,1); % the std of the points that went into each element in pre-shaped "autocov_x" (starting with zero-lag)
-n_cov_xy = zeros(1 + maxlag,2); % how many pairs were finite and how many pairs could have been finite if perfect
+cov_xy = zeros(1 + 2*maxlag,1); % the autocovariance
+cov_std_xy = zeros(1 + 2*maxlag,1); % the std of the points that went into each element in pre-shaped "autocov_x" (starting with zero-lag)
+n_cov_xy = zeros(1 + 2*maxlag,2); % how many pairs were finite and how many pairs could have been finite if perfect
+% There is certainly a way to code this to go in one loop, but this works:
 for i=1:(1 + maxlag) % lag = (i-1)*dt
+    cov_xy_i = zeros(N_x - i + 1,1);
+    for j=1:(N_x - i + 1)
+        cov_xy_i(j) = Y(j)*X(j+i-1);
+    end
+    cov_xy(i+maxlag) = nanmean(cov_xy_i); % autocov
+    cov_std_xy(i+maxlag) = nanstd(cov_xy_i); % std of points that made each element of autocov
+    n_cov_xy(i+maxlag,1) = sum(isfinite(cov_xy_i)); % how many pairs were finite
+    n_cov_xy(i+maxlag,2) = N_x - i + 1; % how many pairs could have been finite if perfect
+end
+for i=2:(1 + maxlag) % lag = -(i-1)*dt
     cov_xy_i = zeros(N_x - i + 1,1);
     for j=1:(N_x - i + 1)
         cov_xy_i(j) = X(j)*Y(j+i-1);
     end
-    cov_xy(i) = nanmean(cov_xy_i); % autocov
-    cov_std_xy(i) = nanstd(cov_xy_i); % std of points that made each element of autocov
-    n_cov_xy(i,1) = sum(isfinite(cov_xy_i)); % how many pairs were finite
-    n_cov_xy(i,2) = N_x - i + 1; % how many pairs could have been finite if perfect
+    cov_xy(maxlag+2-i) = nanmean(cov_xy_i);
+    cov_std_xy(maxlag+2-i) = nanstd(cov_xy_i);
+    n_cov_xy(maxlag+2-i,1) = sum(isfinite(cov_xy_i));
+    n_cov_xy(maxlag+2-i,2) = N_x - i + 1;
 end
 % NOTE: "cov_std_xy" and "n_cov_xy" are unused, and are
-% retained here for troubleshooting and possible inclusion.
+% retained here for troubleshooting and possible inclusion later.
 
-full_cov_xy = [cov_xy;flip(cov_xy(2:(end-1)))];
-% ^ Used in "eval"; this is also what one would use to calculate the
+
+% "cov_xy" is also what one would use to calculate the
 % spectrum with the FFT, but seeing as I already have a function
-% (WKT_spectrum) that does that, I do not have "full_cov_xy" as an output.
+% (WKT_spectrum) that does that, I do not have "cov_xy" as an output.
 % Feel free to modify if desired.
 if strcmp(Window,'blackman') || ...
         strcmp(Window,'flattopwin') || ...
@@ -156,13 +171,13 @@ if strcmp(Window,'blackman') || ...
         strcmp(Window,'hann') || ...
         strcmp(Window,'hanning') || ...
         strcmp(Window,'blackmanharris')
-    Window_ = eval(['fftshift(',Window,'(length(full_cov_xy),''periodic''));']);
+    Window_ = eval([Window,'(length(cov_xy),''periodic'');']);
 else
-    Window_ = eval(['fftshift(',Window,'(length(full_cov_xy)));']);
+    Window_ = eval([Window,'(length(cov_xy));']);
 end
 
-cov_xy = Window_(1:length(cov_xy)).*cov_xy;
-lags = 0:(maxlag);
+cov_xy = Window_.*cov_xy;
+lags = (-maxlag):(maxlag);
 
 if nargout == 0
     ans = cov_xy
@@ -185,3 +200,15 @@ else
 end
 
 end
+
+%% Demonstration and comparison to xcorr:
+
+% X0 = zeros(100,1);
+% for i=2:100; X0(i) = X0(i-1) + 0.9*randn; end
+% X1 = X0 + 0.5*randn(100,1); X1 = wshift('1D',X1,10);
+% X2 = X0 + 0.3*randn(100,1);
+% [C1,lags1] = cov_gaps_window(X1/std(X1),X2/std(X2),'triang');
+% [C2,lags2] = xcorr(X1-mean(X1),X2-mean(X2),'coef');
+% figure;plot(X1,'.-');hold on;plot(X2,'.-')
+% figure;plot(lags1,C1,'.-');hold on;plot(lags2,C2,'.-')
+
