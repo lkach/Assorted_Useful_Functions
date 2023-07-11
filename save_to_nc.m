a67dc4c128b0b18105e6a69527988f9a6b14600b
Luke Kachelein <lkachele@ucsd.edu>
2023-07-11 15:46:50 -0700

    Update save_to_nc.m
    Added a section and new variable to provide instructions for deleting a variable from a netCDF file.

--- save_to_nc.m
+++ save_to_nc.m
@@ -1,4 +1,6 @@
-% Save one new variable into a netCDF file in one step
+% Save one new variable into a netCDF file in one step. This can overwrite
+% any pre-existing variables of the same name given the right option is
+% selected.
 % 
 % IN:
 %   FILE = string, full file path of .nc file
@@ -11,7 +13,11 @@
 %          can only write vectors here)
 %   METADATA = arbitrarily long cell comprised of length-2 cells of
 %          attribute-value pairs, e.g. {'Units','m/s'}
-% 
+%   OVERWRITE = (default "false") prints the system command needed to
+%          delete the variable in question if it already exists. MATLAB has
+%          the native "system" function but that does not seem to work with
+%          commands that are installed by the user, only native system
+%          commands. This variable may be either "true" or "false" (or 1 or 0).
 % 
 % 
 % Examples of the more complicated inputs:
@@ -20,8 +26,27 @@
 % the file.
 % METADATA = {{'Attribute1','Value1'},{'Attribute2','Value2'},...}
 
-function save_to_nc(FILE,NAME,DATA,DIM_METADATA,METADATA)
+function save_to_nc(FILE,NAME,DATA,DIM_METADATA,METADATA,varargin)
 % Open the existing netCDF file in write mode
+
+if nargin == 5
+    OVERWRITE = false;
+elseif nargin == 6
+    OVERWRITE = varargin{1};
+else
+    error(['The function save_to_nc takes 5 or 6 input arguments, while here ' num2str(nargin) ' were given.'])
+end
+
+if OVERWRITE
+    disp(' ')
+    disp(['The following is the bash command for deleting the variable "' NAME '" from the file.'])
+    disp(['This requires the nco package to be installed.'])
+    disp(' ')
+    disp(['ncks -x -v ' NAME ' ' FILE ' ' FILE]);
+    disp(' ')
+else
+end
+
 NCID = netcdf.open(FILE, 'WRITE');
 
 % Get all dimensions already present in the netCDF file:
@@ -32,6 +57,26 @@ PRIOR_DIM_SIZE  = cell(NUM_PRIOR_DIMS,1);
 for ii = 1:NUM_PRIOR_DIMS
     [PRIOR_DIM_NAMES{ii},PRIOR_DIM_SIZE{ii}] = netcdf.inqDim(NCID,PRIOR_DIM_IDS(ii));
 end
+
+% % Get names of existing variables:
+% EXVARIDS = netcdf.inqVarIDs(NCID); EXVARNAMES = cell(size(EXVARIDS));
+% for ii = 1:length(EXVARIDS)
+%     EXVARNAMES{ii} = netcdf.inqVar(NCID,EXVARIDS(ii));
+% end
+% % Identify the index of the variable to delete
+% for ii = 1:length(EXVARIDS)
+%     if strcmp(NAME,EXVARNAMES{ii})
+%         DELVARIND = EXVARIDS(ii);
+%     else
+%     end
+% end
+% % Delete the existing variable
+% if ~isempty(DELVARIND)
+%     netcdf.reDef(NCID);
+%     cdflib.deleteVar(NCID, DELVARIND);
+%     netcdf.endDef(NCID);
+% else
+% end
 
 % Define DIMIDS
 IS_EXISTING_DIM = zeros(size(PRIOR_DIM_NAMES));
