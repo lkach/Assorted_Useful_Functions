7a5417432d665be95a5fe1916d0bc54a51248126
Luke Kachelein <lukachelein@gmail.com>
2026-08-17 18:51:27 -0700

    Update ncdisp_short.m

--- ncdisp_short.m
+++ ncdisp_short.m
@@ -1,7 +1,7 @@
 % NCDISP_SHORT(NC_FILE)
 % 
 % Takes the string of a netCDF file's path as input and outputs a simple
-% display of the variables names and dimensions in two columns. No
+% display of the variables' names and dimensions in two columns. No
 % overwhelming metadata to sort through, just the essential values you want
 % to know most of the time.
 
